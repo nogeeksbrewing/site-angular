7afb41200ca8809ac75066d37f3c6d07d4d8a153
Allan Lewis <allan@nogeeksbrewing.com>
2022-08-03 04:07:12 -0400

    Closes #8 - Brewery component inline

--- src/app/brewery/brewery.component.ts
+++ src/app/brewery/brewery.component.ts
@@ -5,8 +5,74 @@ import { Brewery } from '../objects';
 
 @Component({
   selector: 'app-brewery',
-  templateUrl: './brewery.component.html',
-  styleUrls: ['./brewery.component.scss']
+  template: `
+  <div *ngIf="(brewery | async) as brewery" id="brewery-container">
+    <header>
+      <h1>{{brewery.name}}</h1>
+    </header>
+    <main>
+      <section><app-batches-list></app-batches-list></section>
+    </main>
+    <footer>
+      <div id="brewery-footer-wrapper">
+        <div id="brewery-bottom-left"><h5>{{brewery.location}}</h5></div>
+        <div id="brewery-bottom-middle"><h5><a href="mailto:{{brewery.contact}}">{{brewery.contact}}</a></h5></div>
+        <div id="brewery-bottom-right"><h5>{{brewery.info}}</h5></div>
+      </div>
+    </footer>
+  </div>
+  `,
+  styles: [`
+    #brewery-container {
+      display: flex; /* displays flex-items (children) inline */
+      flex-direction: column; /* stacks them vertically */
+      height: 100%; /* needs to take the parents height, alternative: body {display: flex} */
+    }
+    
+    main {
+      height: 100%;
+      flex: 1; /* takes the remaining height of the "container" div */
+      overflow: auto; /* to scroll just the "main" div */
+      background: #585858;
+    }
+    
+    section {
+      overflow: auto; /* recommended */
+    }
+    
+    header {
+      background: #383838;
+      text-align: center;
+    }
+    
+    footer {
+      color: #F5F5F5;
+      background: #383838;
+    }
+    
+    #brewery-footer-wrapper {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content:  space-between;
+      align-items: stretch;
+    }
+    
+    #brewery-bottom-left {
+      text-align: left;
+      padding-left: 16px;
+    }
+    
+    #brewery-bottom-middle {
+      text-align: center;
+    }
+    
+    #brewery-bottom-right {
+      text-align: right;
+      padding-right: 16px;
+    }
+    `
+  ]
 })
 export class BreweryComponent implements OnInit {
 
